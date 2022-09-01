5970bc839c02ad3091ffb49bbffa056ba6bb5662
Ricardo Dias <rdias@nuxeo.com>
2022-09-01 10:17:06 +0100

    WEBUI-730: update catalog generator workflow

--- packages/nuxeo-designer-catalog/scripts/maven-deploy.sh
+++ packages/nuxeo-designer-catalog/scripts/maven-deploy.sh
@@ -17,7 +17,7 @@
 
 # Run in a clean directory passing in a GitHub org, repo and tag/branch name
 if [ "$#" -ne 2 ]; then
-    echo "maven-deploy.sh <root> <verion>"
+    echo "maven-deploy.sh <root> <version>"
     exit 1
 fi
 
@@ -26,12 +26,8 @@ VERSION=$2
 GROUPID=org.nuxeo.web.ui.studio
 ARTIFACTID=view-designer-catalog
 
-MVN_REPO_ID=public-releases
-MVN_REPO_URL=http://mavenin.nuxeo.com/nexus/content/repositories/public-releases
-if [[ $VERSION == *-SNAPSHOT ]]; then
-    MVN_REPO_ID=public-snapshots
-    MVN_REPO_URL=http://mavenin.nuxeo.com/nexus/content/repositories/public-snapshots
-fi
+MVN_REPO_ID=maven-public
+MVN_REPO_URL=http://packages.nuxeo.com/nexus/content/repositories/maven-public
 
 pushd $ROOT
 zip -r $ARTIFACTID-$VERSION.zip nuxeo-web-ui data hints catalog.json
